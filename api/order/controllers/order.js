179e4eea945deca244abe18b048905f8e376de94
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-31 20:32:13 -0700

    add email hooks to confirmation process after an order is created

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -58,7 +58,6 @@ module.exports = {
   },
   create: async (ctx) => {
     const { paymentIntent, name, email, cart } = ctx.request.body;
-    console.log({paymentIntent})
     let paymentInfo;
     // payment intent for validation
     try {
@@ -103,7 +102,7 @@ module.exports = {
       0
     );
     // check payment amt is consistent (stripe payments are in cents)
-    if (paymentInfo.amount !== (total_in_dollars * 100)) {
+    if (paymentInfo.amount !== total_in_dollars * 100) {
       ctx.response.status = 402;
       return { error: "The total to be paid is different from Payment Intent" };
     }
@@ -119,6 +118,56 @@ module.exports = {
     };
 
     const entity = await strapi.services.order.create(entry);
+    const order_id = `${entity.id}-${entity.orderId.substring(
+      entity.orderId.length - 5
+    )}`;
+    const table_style =
+      "border-radius: 3px;";
+    const order_table = sanitizedCart
+      .map(
+        (item) =>
+          `<tr>
+        <td style=${table_style}>${item.title}</td>
+        <td style=${table_style}>${item.price}.00</td>
+        <td style=${table_style}>${item.qty}</td>
+        </tr>`
+      )
+      .join("");
+
+    // Send confirmation email to purchaser
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: email,
+        bcc: "dharmic.astrologicalsystems@gmail.com",
+        subject: "Thank you for your order",
+        text: `Thank you for your order. Your order is number ${order_id}. Someone will be in touch within three business days to schedule an appointment. Please reply to this email with any questions.`,
+        html: `<h4>Hello ${name},</h4>
+          <p>We thank you for your recent order.</p>
+          <table width="600" style="border-top: 1px solid grey; border-bottom: 1px solid grey">
+            <thead>
+              <tr>
+                <th style="text-align:left; border-bottom: .5px solid grey">Order</th>
+                <th style="text-align:left; border-bottom: .5px solid grey">Cost (each)</th>
+                <th style="text-align:left; border-bottom: .5px solid grey">Qty</th>
+              </tr>
+            </thead>
+            <tbody>
+                ${order_table}
+            </tbody>
+          </table>
+          <span style="text-align:right; margin-top: 10px">Total: $${entity.total_in_dollars}.00</span>
+          <p>Your order number is ${order_id}</p>
+          <p>Someone will be in touch soon to schedule your appointment. Please reach out to us at dharmic.astrologicalsystems@gmail.com if you do not hear from us within three business days.</p>
+          <div>Sincerely,</div>
+          <div>Dharmic Astrology</div>
+          <a href="https://www.dharmicastrology.com">www.dharmicastrology.com</a>
+        `,
+      });
+    } catch (err) {
+      console.log("Exception in sending err", err);
+    }
+
+    // finally return data to client
     return sanitizeEntity(entity, { model: strapi.models.order });
   },
 };
